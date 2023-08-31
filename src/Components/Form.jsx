4ebd21dc104feb9d97d149b68e5ca5a81f4cbacd
your_username <dannyclassic56@gmail.com>
2023-08-31 18:39:52 +0100

    Changed from 1-100 to A-B

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
+const grades = ["A", "B", "C", "D", "E", "F"];
 
 const Form = (props) => {
   const [unit, setUnit] = useState(1);
   const [courseSub, setCourse] = useState("");
-  const [totalScore, setTotal] = useState(100);
+  const [grade, setGrade] = useState("A");
+  // const [totalScore, setTotal] = useState(100);
 
   const calculateTotal = (score) => {
-    if (score >= 70) {
+    if (score === "A") {
       return 5;
-    } else if (score >= 60) {
+    } else if (score === "B") {
       return 4;
-    } else if (score >= 50) {
+    } else if (score === "C") {
       return 3;
-    } else if (score >= 45) {
+    } else if (score === "D") {
       return 2;
-    } else if (score >= 40) {
+    } else if (score === "E") {
       return 1;
     } else {
       return 0;
     }
   };
+  // const calculateTotal = (score) => {
+  //   if (score >= 70) {
+  //     return 5;
+  //   } else if (score >= 60) {
+  //     return 4;
+  //   } else if (score >= 50) {
+  //     return 3;
+  //   } else if (score >= 45) {
+  //     return 2;
+  //   } else if (score >= 40) {
+  //     return 1;
+  //   } else {
+  //     return 0;
+  //   }
+  // };
   function handleSUbmit(e) {
     e.preventDefault();
 
     if (!courseSub) return;
-    const total = calculateTotal(totalScore);
+    const total = calculateTotal(grade);
     const course = courseSub.toUpperCase();
     const newItem = {
       course,
@@ -38,7 +55,7 @@ const Form = (props) => {
     props.onAddItem(newItem);
     setUnit(1);
     setCourse("");
-    setTotal(100);
+    setGrade("A");
   }
 
   return (
@@ -57,20 +74,30 @@ const Form = (props) => {
             value={unit}
             onChange={(e) => setUnit(Number(e.target.value))}
           >
-            {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+            {Array.from({ length: 4 }, (_, i) => i + 1).map((num) => (
               <option value={num} key={num}>
                 {num}
               </option>
             ))}
           </select>
         </div>
-        <input
+        <div className="unit">
+          <p>Grade:</p>
+          <select value={grade} onChange={(e) => setGrade(e.target.value)}>
+            {grades.map((num) => (
+              <option value={num} key={num}>
+                {num}
+              </option>
+            ))}
+          </select>
+        </div>
+        {/* <input
           value={totalScore}
           type="number"
           max={100}
           placeholder="Enter Course Score"
           onChange={(e) => setTotal(Number(e.target.value))}
-        />
+        /> */}
         <button className="add">Add</button>
       </div>
     </form>
